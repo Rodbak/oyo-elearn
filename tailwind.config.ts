a5e61a01ea8548060a078fb73bd7c82514b40e85
Rodbak <160141463+Rodbak@users.noreply.github.com>
2026-06-16 12:02:02 +0000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "#000000",
+        background: "#e9e9e9",
         foreground: "#1E293B",
         muted: "#272727",
         accent: {
